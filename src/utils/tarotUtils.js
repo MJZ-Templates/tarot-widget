85fd1af0b846b1e05cd68f55f2fafea567c74663
misung-dev <misung.dev@gmail.com>
2025-03-10 09:29:10 +0900

    feat: 카드 해석 추가

--- src/utils/tarotUtils.js
+++ src/utils/tarotUtils.js
@@ -4,7 +4,7 @@ export const TAROT_CARD_LIST = [
     meaning: "New beginnings and possibilities",
     image: "../../src/assets/images-tarot-cards/00_Fool.png",
     description:
-      "The Fool represents unlimited potential and new journeys. It signifies spontaneity and stepping into the unknown with trust. This card often symbolizes a fresh start, a leap of faith, or personal growth.",
+      "The Fool represents unlimited potential and new journeys. It signifies spontaneity and stepping into the unknown with trust. This card is a call to embrace adventure and live in the moment. The Fool’s journey reminds us that we must occasionally take risks to reap rewards.",
     tag: ["beginnings", "adventure", "trust"],
   },
   {
@@ -12,7 +12,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Creativity and resourcefulness",
     image: "../../src/assets/images-tarot-cards/01_Magician.png",
     description:
-      "The Magician symbolizes mastery, skill, and the ability to manifest one's desires. It encourages action, confidence, and using available resources to create opportunities. This card represents taking control of one's destiny.",
+      "The Magician symbolizes mastery, skill, and the ability to manifest one's desires. It encourages action, confidence, and using available resources to create opportunities. The Magician is about making things happen and confidently executing one's plans. It underlines the importance of turning thoughts into actions.",
     tag: ["manifestation", "power", "action"],
   },
   {
@@ -20,7 +20,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Intuition and inner wisdom",
     image: "../../src/assets/images-tarot-cards/02_High_Priestess.png",
     description:
-      "The High Priestess represents deep knowledge, intuition, and mystery. She advises patience and listening to one's inner voice. This card suggests that not everything is meant to be understood immediately.",
+      "The High Priestess represents deep knowledge, intuition, and mystery. She advises patience and listening to one's inner voice. The High Priestess’s wisdom comes from quiet reflection and a deep understanding of the unseen world. This card inspires trust in one's inner guidance and the power of inner knowing.",
     tag: ["intuition", "mystery", "wisdom"],
   },
   {
@@ -28,7 +28,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Abundance and creativity",
     image: "../../src/assets/images-tarot-cards/03_Empress.png",
     description:
-      "The Empress embodies fertility, nurturing energy, and growth. She is associated with nature, love, and artistic expression. This card indicates a time of prosperity and emotional fulfillment.",
+      "The Empress embodies fertility, nurturing energy, and growth. She is associated with nature, love, and artistic expression. The Empress signifies a time when nurturing and embracing creativity can bring prosperity. This card calls for appreciating the beauty in life and the bounty it offers.",
     tag: ["growth", "nurturing", "abundance"],
   },
   {
@@ -36,7 +36,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Authority and stability",
     image: "../../src/assets/images-tarot-cards/04_Emperor.png",
     description:
-      "The Emperor symbolizes structure, leadership, and control. He represents order, discipline, and a strong foundation. This card suggests taking responsibility and asserting one's power.",
+      "The Emperor symbolizes structure, leadership, and control. He represents order, discipline, and a strong foundation. This card is about asserting control and providing stability in one's life. The Emperor’s wisdom is in the benefits of order and the power of responsibility.",
     tag: ["stability", "leadership", "discipline"],
   },
   {
@@ -44,7 +44,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Tradition and spiritual guidance",
     image: "../../src/assets/images-tarot-cards/05_Hierophant.png",
     description:
-      "The Hierophant represents spiritual wisdom, rituals, and established beliefs. It suggests seeking knowledge from mentors or traditions. This card encourages adherence to structure and faith.",
+      "The Hierophant represents spiritual wisdom, rituals, and established beliefs. It suggests seeking knowledge from mentors or traditions. The Hierophant card encourages learning and the adherence to spiritual practices. It is a reminder of the value of traditions and the insights they can offer.",
     tag: ["tradition", "wisdom", "guidance"],
   },
   {
@@ -52,7 +52,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Love and harmony",
     image: "../../src/assets/images-tarot-cards/06_Lovers.png",
     description:
-      "The Lovers card represents relationships, choices, and alignment. It signifies deep connections and moral dilemmas. This card urges balance and considering the heart's true desires.",
+      "The Lovers card represents relationships, choices, and alignment. It signifies deep connections and moral dilemmas. This card encourages finding balance in personal relationships and making choices that align with one's values. The Lovers also highlight the importance of harmony and mutual respect in partnerships.",
     tag: ["relationships", "harmony", "choice"],
   },
   {
@@ -60,7 +60,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Willpower and victory",
     image: "../../src/assets/images-tarot-cards/07_Chariot.png",
     description:
-      "The Chariot symbolizes determination, control, and overcoming obstacles. It represents triumph through focus and self-discipline. This card suggests a strong will and moving forward fearlessly.",
+      "The Chariot symbolizes determination, control, and overcoming obstacles. It represents triumph through focus and self-discipline. This card is a call to harness inner strength and keep moving forward fearlessly. The Chariot's message is about mastering one's emotions and steering life towards success.",
     tag: ["determination", "success", "control"],
   },
   {
@@ -68,7 +68,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Inner strength and courage",
     image: "../../src/assets/images-tarot-cards/08_Strength.png",
     description:
-      "Strength represents resilience, patience, and compassion. It shows power through gentleness and inner fortitude. This card suggests overcoming challenges with grace and self-control.",
+      "Strength represents resilience, patience, and compassion. It shows power through gentleness and inner fortitude. This card highlights the importance of soft power and compassion in overcoming life's challenges. Strength encourages using patience and grace to navigate difficult situations.",
     tag: ["courage", "patience", "compassion"],
   },
   {
@@ -76,7 +76,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Solitude and reflection",
     image: "../../src/assets/images-tarot-cards/09_Hermit.png",
     description:
-      "The Hermit symbolizes introspection and seeking deeper truth. It encourages self-discovery and taking time for contemplation. This card often appears when one is on a personal journey of understanding.",
+      "The Hermit symbolizes introspection and seeking deeper truth. It encourages self-discovery and taking time for contemplation. This card is about finding wisdom through solitude and deep reflection. The Hermit advises looking within to understand complex life questions and personal goals.",
     tag: ["introspection", "wisdom", "solitude"],
   },
   {
@@ -84,7 +84,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Fate and change",
     image: "../../src/assets/images-tarot-cards/10_Wheel_of_Fortune.png",
     description:
-      "This card represents life's ups and downs, destiny, and cycles. It reminds us that change is constant and encourages us to embrace the unexpected.",
+      "This card represents life's ups and downs, destiny, and cycles. It reminds us that change is constant and encourages us to embrace the unexpected. The Wheel of Fortune signifies the turning point in life and the cyclical nature of fate. This card suggests being open to change and embracing it as a part of life's journey.",
     tag: ["fate", "change", "cycles"],
   },
   {
@@ -92,7 +92,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Balance and fairness",
     image: "../../src/assets/images-tarot-cards/11_Justice.png",
     description:
-      "Justice symbolizes truth, integrity, and accountability. It encourages making fair decisions and taking responsibility for actions. This card signifies the law of cause and effect.",
+      "Justice symbolizes truth, integrity, and accountability. It encourages making fair decisions and taking responsibility for actions. This card calls for an examination of fairness and encourages self-honesty. Justice serves as a reminder that consequences are inevitable for one's actions.",
     tag: ["fairness", "truth", "balance"],
   },
   {
@@ -100,7 +100,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Sacrifice and new perspective",
     image: "../../src/assets/images-tarot-cards/12_Hanged_Man.png",
     description:
-      "The Hanged Man signifies surrender, patience, and a shift in perspective. It encourages looking at things from a new angle and letting go of control.",
+      "The Hanged Man signifies surrender, patience, and a shift in perspective. It encourages looking at things from a new angle and letting go of control. This card advises a pause and reassessment of one's situation and goals. The Hanged Man is a reminder that patience and change in viewpoint can bring clarity.",
     tag: ["perspective", "surrender", "patience"],
   },
   {
@@ -108,7 +108,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Transformation and new beginnings",
     image: "../../src/assets/images-tarot-cards/13_Death.png",
     description:
-      "Death represents endings that lead to fresh starts. It is a card of rebirth, transition, and personal evolution. Embrace change as an opportunity for growth.",
+      "Death represents endings that lead to fresh starts. It is a card of rebirth, transition, and personal evolution. Embrace change as an opportunity for growth and transformation. This card encourages letting go of the old to make way for new beginnings.",
     tag: ["transformation", "endings", "rebirth"],
   },
   {
@@ -116,7 +116,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Balance and moderation",
     image: "../../src/assets/images-tarot-cards/14_Temperance.png",
     description:
-      "Temperance symbolizes harmony, patience, and self-restraint. It encourages finding middle ground and integrating different aspects of life.",
+      "Temperance symbolizes harmony, patience, and self-restraint. It encourages finding middle ground and integrating different aspects of life. This card is about moderation and the blending of opposites to create balance. Temperance suggests a careful approach to decisions and interactions.",
     tag: ["harmony", "moderation", "patience"],
   },
   {
@@ -124,7 +124,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Desires and limitations",
     image: "../../src/assets/images-tarot-cards/15_Devil.png",
     description:
-      "The Devil represents attachments, temptations, and self-imposed limitations. It serves as a reminder to break free from unhealthy patterns.",
+      "The Devil represents attachments, temptations, and self-imposed limitations. It serves as a reminder to break free from unhealthy patterns. This card highlights the importance of acknowledging and overcoming one's shadow side. The Devil urges self-awareness and liberation from destructive behaviors.",
     tag: ["temptation", "attachment", "control"],
   },
   {
@@ -132,7 +132,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Sudden change",
     image: "../../src/assets/images-tarot-cards/16_Tower.png",
     description:
-      "The Tower symbolizes disruption and awakening. It brings unexpected transformation that can lead to breakthroughs and rebuilding stronger foundations.",
+      "The Tower symbolizes disruption and awakening. It brings unexpected transformation that can lead to breakthroughs and rebuilding stronger foundations. This card suggests that upheavals, though challenging, are opportunities for profound growth. The Tower's lesson is about resilience and the positive outcomes of rebuilding from the ground up.",
     tag: ["change", "chaos", "rebuilding"],
   },
   {
@@ -140,7 +140,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Hope and inspiration",
     image: "../../src/assets/images-tarot-cards/17_Star.png",
     description:
-      "The Star represents optimism, faith, and guidance. It encourages trusting the universe and following dreams with hope.",
+      "The Star represents optimism, faith, and guidance. It encourages trusting the universe and following dreams with hope. This card inspires renewal and a clearer vision for the future. The Star's light guides one towards fulfillment and encourages optimism in challenges.",
     tag: ["hope", "faith", "inspiration"],
   },
   {
@@ -148,7 +148,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Illusion and intuition",
     image: "../../src/assets/images-tarot-cards/18_Moon.png",
     description:
-      "The Moon symbolizes uncertainty, dreams, and intuition. It calls for trusting inner instincts and seeing beyond illusions.",
+      "The Moon symbolizes uncertainty, dreams, and intuition. It calls for trusting inner instincts and seeing beyond illusions. This card invites exploration of the subconscious and the mysteries it holds. The Moon is a reminder to listen to intuitive insights and embrace the unknown.",
     tag: ["intuition", "mystery", "uncertainty"],
   },
   {
@@ -156,7 +156,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Happiness and success",
     image: "../../src/assets/images-tarot-cards/19_Sun.png",
     description:
-      "The Sun radiates joy, vitality, and clarity. It represents achievements, positivity, and enlightenment.",
+      "The Sun radiates joy, vitality, and clarity. It represents achievements, positivity, and enlightenment. This card signifies a time of contentment, success, and vibrant energy. The Sun encourages celebrating life's victories and embracing its warmth.",
     tag: ["happiness", "success", "clarity"],
   },
   {
@@ -164,7 +164,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Awakening and renewal",
     image: "../../src/assets/images-tarot-cards/20_Judgement.png",
     description:
-      "Judgement signifies reflection, forgiveness, and transformation. It calls for making life-changing decisions with awareness.",
+      "Judgement signifies reflection, forgiveness, and transformation. It calls for making life-changing decisions with awareness. This card encourages self-evaluation and letting go of past mistakes. Judgement is a key to personal accountability and the process of renewal.",
     tag: ["awakening", "rebirth", "reflection"],
   },
   {
@@ -172,7 +172,7 @@ export const TAROT_CARD_LIST = [
     meaning: "Completion and fulfillment",
     image: "../../src/assets/images-tarot-cards/21_World.png",
     description:
-      "The World represents wholeness, achievements, and closure. It signifies reaching the end of a journey and celebrating accomplishments.",
+      "The World represents wholeness, achievements, and closure. It signifies reaching the end of a journey and celebrating accomplishments. This card marks the completion of a significant cycle and the joy of fulfillment. The World encourages honoring one's growth and the journey taken.",
     tag: ["completion", "fulfillment", "achievement"],
   },
 ];
